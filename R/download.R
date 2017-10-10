c63e5bf183fc442f5d1efe78216fbeee9d642489
Jeroen <jeroenooms@gmail.com>
2017-10-10 14:51:43 +0200

    Improve error message

--- R/download.R
+++ R/download.R
@@ -2,8 +2,15 @@ download <- function(url){
   h <- curl::new_handle()
   curl::handle_setheaders(h, "Accept" = "application/ld+json, application/json")
   req <- curl::curl_fetch_memory(url, handle = h)
-  if(req$status >= 400)
-    stop(sprintf("Download (HTTP %d): %s ", req$status, url), call. = FALSE)
+  if(req$status >= 400){
+    json <- rawToChar(req$content)
+    if(jsonlite::validate(json)){
+      data <- jsonlite::fromJSON(json)
+      if(length(data$message))
+        stop(sprintf("(%s) %s", url, data$message))
+    }
+    stop(sprintf("Download (HTTP %d): %s", req$status, url))
+  }
   headers <- curl::parse_headers(req$headers)
   get_header <- function(name){
     val <- grep(paste0("^", name, "[ \t]*:"), headers, ignore.case = TRUE, value = TRUE)
