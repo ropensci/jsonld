e37f9bdecd6d1002f48d987e61a93bbe5d175030
Jeroen <jeroenooms@gmail.com>
2018-04-14 14:51:32 +0200

    Try with jsonld.js 1.0.1

--- R/onload.R
+++ R/onload.R
@@ -1,7 +1,7 @@
 #' @importFrom V8 new_context
 .onLoad <- function(lib, pkg){
   assign("ctx", V8::v8("window"), environment(.onLoad))
-  ctx$source(system.file("js/jsonld.js", package = pkg))
+  ctx$source(system.file("js/jsonld.min.js", package = pkg))
   #ctx$source(system.file("js/es6-promise.js", package = pkg))
   ctx$source(system.file("js/wrappers.js", package = pkg))
 }
